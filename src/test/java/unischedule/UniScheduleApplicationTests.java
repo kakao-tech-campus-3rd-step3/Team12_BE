457f750b949bb236ceded7cc731bd188232e8d2c
Junyoung Yang <y@jun0.dev>
2025-08-30 18:35:55 +0900

    feat: 스프링 부트 프로젝트 초기 세팅 (#2)

---
+++ src/test/java/unischedule/UniScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package unischedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
