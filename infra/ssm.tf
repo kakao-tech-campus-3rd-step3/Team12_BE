057c842f695f730381407015a727b6bf71c50ccd
hardjava <fkdlcn123@kumoh.ac.kr>
2025-09-04 15:30:04 +0900

    feat(infra): 버전 제약 추가 및 DB 비밀번호 Output 제거

--- infra/ssm.tf
+++ infra/ssm.tf
@@ -2,26 +2,7 @@ data "aws_ssm_parameter" "db_username" {
   name = "/uni-schedule/config/db_username"
 }
 
-output "db_username_value" {
-  value     = data.aws_ssm_parameter.db_username.value
-  sensitive = true
-}
-
 data "aws_ssm_parameter" "db_password" {
   name            = "/uni-schedule/config/db_password"
   with_decryption = true
-}
-
-output "db_password_value" {
-  value     = data.aws_ssm_parameter.db_password.value
-  sensitive = true
-}
-
-data "aws_ssm_parameter" "account_id" {
-  name = "/user-config/account-id"
-}
-
-output "account_id_value" {
-  value     = data.aws_ssm_parameter.account_id.value
-  sensitive = true
 }
