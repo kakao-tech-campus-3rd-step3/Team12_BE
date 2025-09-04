05ad7f0d93be09351836e1a2e1bd2afbde59ec42
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-09-04 16:13:05 +0900

    fix(test): Entity id auto increment 설정 #5

--- backend/src/main/java/unischedule/users/entity/Calendar.java
+++ backend/src/main/java/unischedule/users/entity/Calendar.java
@@ -2,6 +2,8 @@ package unischedule.users.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -13,6 +15,7 @@ import java.util.List;
 @Table(name="calendars")
 public class Calendar {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="calendar_id")
     private Long id;
     
--- backend/src/main/java/unischedule/users/entity/Event.java
+++ backend/src/main/java/unischedule/users/entity/Event.java
@@ -2,6 +2,8 @@ package unischedule.users.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Event {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="event_id")
     private Long id;
     
