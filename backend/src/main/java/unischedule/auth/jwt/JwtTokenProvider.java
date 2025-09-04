b125ec1183ec881e4ef6b5c3abdde945281ff08e
sinsehwan <sk124590@gmail.com>
2025-09-04 13:23:27 +0900

    feat(auth): implement jwtAuthenticationFilter & get authentication logic #6

--- backend/src/main/java/unischedule/auth/jwt/JwtTokenProvider.java
+++ backend/src/main/java/unischedule/auth/jwt/JwtTokenProvider.java
@@ -4,12 +4,18 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 
@@ -59,6 +65,20 @@ public class JwtTokenProvider {
         catch (Exception ex) {
             return false;
         }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get("auth")
+                .toString()
+                .split(", "))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 
     public String getEmail(String token) {
