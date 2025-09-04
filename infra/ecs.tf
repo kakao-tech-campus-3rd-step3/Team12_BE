f12aee327481d8fab649a391a5f4382d46283dcc
hardjava <fkdlcn123@kumoh.ac.kr>
2025-09-04 18:11:10 +0900

    feat(infra): ECS 클러스터 및 서비스 구성 추가

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -1,48 +1,14 @@
-resource "aws_security_group" "ecs_instance" {
-  vpc_id = aws_vpc.this.id
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb.id]
-  }
-
-  ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb.id]
-  }
-
-  ingress {
-    description = "SSH access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_ecs_cluster" "this" {
-  name = "ticketing-cluster"
+  name = "uni-schedule-cluster"
 }
 
 resource "aws_ecs_task_definition" "this" {
-  family                   = "ticketing-task"
+  family                   = "uni-schedule-task"
   requires_compatibilities = ["FARGATE"]
-  network_mode            = "awsvpc"
-  cpu                     = "512"
-  memory                  = "1024"
-  execution_role_arn      = aws_iam_role.ecs_task_execution.arn
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
   runtime_platform {
     cpu_architecture        = "ARM64"
@@ -52,38 +18,52 @@ resource "aws_ecs_task_definition" "this" {
   container_definitions = jsonencode([
     {
       name      = "backend"
-      image     = var.ecr_image,
+      image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.ap-northeast-2.amazonaws.com/backend:latest",
       essential = true
+
       portMappings = [
         {
           containerPort = 8080
           protocol      = "tcp"
         }
       ]
+
+      environment = [
+        { name = "SPRING_DATASOURCE_URL", value = data.aws_ssm_parameter.db_url.value },
+        { name = "SPRING_DATASOURCE_USERNAME", value = data.aws_ssm_parameter.db_username.value },
+        { name = "SPRING_DATASOURCE_PASSWORD", value = data.aws_ssm_parameter.db_password.value }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.backend.name,
+          "awslogs-region"        = data.aws_region.current.name,
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
     }
   ])
-
 }
 
-# ALB
 resource "aws_lb" "this" {
-  name               = "ticketing-lb"
+  name               = "uni-schedule-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb.id]
-  subnets            = aws_subnet.public[*].id
+  subnets            = aws_subnet.public_subnet[*].id
 }
 
 resource "aws_lb_target_group" "this" {
-  name        = "ticketing-tg"
-  port        = 8080  #80
+  name        = "uni-schedule-tg"
+  port        = 8080 #80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.this.id
   target_type = "ip"
 
   health_check {
-    path                = "/healthcheck"
-    matcher             = "200-499"
+    path                = "/actuator/health"
+    matcher             = "200-399"
     interval            = 30
     timeout             = 20 #10
     healthy_threshold   = 2
@@ -104,14 +84,15 @@ resource "aws_lb_listener" "this" {
 
 # ECS Service
 resource "aws_ecs_service" "this" {
-  name            = "ticketing-service"
-  cluster         = aws_ecs_cluster.this.id
-  task_definition = aws_ecs_task_definition.this.arn
-  desired_count   = 2
-  launch_type     = "FARGATE"
-  
+  name                              = "uni-schedule-service"
+  cluster                           = aws_ecs_cluster.this.id
+  task_definition                   = aws_ecs_task_definition.this.arn
+  desired_count                     = 2
+  launch_type                       = "FARGATE"
+  health_check_grace_period_seconds = 300
+
   network_configuration {
-    subnets          = aws_subnet.public[*].id
+    subnets          = aws_subnet.public_subnet[*].id
     security_groups  = [aws_security_group.ecs_instance.id]
     assign_public_ip = true
   }
@@ -123,4 +104,13 @@ resource "aws_ecs_service" "this" {
   }
 
   depends_on = [aws_lb_listener.this]
+}
+
+resource "aws_cloudwatch_log_group" "backend" {
+  name              = "/ecs/backend"
+  retention_in_days = 14
+
+  tags = {
+    Name = "backend-log-group"
+  }
 }
