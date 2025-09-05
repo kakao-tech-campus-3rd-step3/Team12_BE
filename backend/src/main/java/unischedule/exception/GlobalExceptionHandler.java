f0e0ff0141538d83822a6280d1762cc706452980
Junyoung Yang <y@jun0.dev>
2025-09-05 10:56:12 +0900

    feat(exception): GlobalExceptionHandler 및 각종 Custom Exception 추가

---
+++ backend/src/main/java/unischedule/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package unischedule.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import unischedule.exception.dto.ErrorResponseDto;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleEntityNotFound(EntityNotFoundException ex) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(InvalidInputException.class)
+    public ResponseEntity<ErrorResponseDto> handleInvalidInput(InvalidInputException ex) {
+        return ResponseEntity.badRequest().body(ErrorResponseDto.of(ex.getMessage()));
+    }
+
+    @ExceptionHandler(ExternalApiException.class)
+    public ResponseEntity<ErrorResponseDto> handleEntityNotFound(ExternalApiException ex) {
+        return ResponseEntity.internalServerError().body(new ErrorResponseDto(ex.getMessage()));
+    }
+}
+
---
+++ backend/src/main/java/unischedule/exception/InvalidInputException.java
@@ -0,0 +1,9 @@
+package unischedule.exception;
+
+public class InvalidInputException extends RuntimeException {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
+
