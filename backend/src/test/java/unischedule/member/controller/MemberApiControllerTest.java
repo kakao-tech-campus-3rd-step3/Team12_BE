32203205b661a841c81fc30f4a902b469e3f3b51
sinsehwan <sk124590@gmail.com>
2025-09-11 14:07:05 +0900

    chore(exception): add exceptionHandler & chore test code in member #22

--- backend/src/test/java/unischedule/member/controller/MemberApiControllerTest.java
+++ backend/src/test/java/unischedule/member/controller/MemberApiControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import unischedule.auth.jwt.JwtTokenProvider;
+import unischedule.exception.dto.EntityAlreadyExistsException;
 import unischedule.member.dto.LoginRequestDto;
 import unischedule.member.dto.MemberRegistrationDto;
 import unischedule.member.service.MemberService;
@@ -21,8 +22,10 @@ import unischedule.member.service.MemberService;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,7 +53,6 @@ class MemberApiControllerTest {
     void signup() throws Exception {
         // given
         MemberRegistrationDto registrationDto = new MemberRegistrationDto("test12@gmail.com", "nickname", "p1a2s3s4word");
-        given(memberService.isMemberExists(registrationDto.email())).willReturn(false);
         doNothing().when(memberService).registerMember(any(MemberRegistrationDto.class));
 
         // when & then
@@ -66,14 +68,17 @@ class MemberApiControllerTest {
     void signupDuplicated() throws Exception {
         // given
         MemberRegistrationDto registrationDto = new MemberRegistrationDto("test12@gmail.com", "nickname", "p1a2s3s4word");
-        given(memberService.isMemberExists(registrationDto.email())).willReturn(true);
+
+        doThrow(new EntityAlreadyExistsException("이미 사용중인 이메일입니다."))
+                .when(memberService)
+                .registerMember(any(MemberRegistrationDto.class));
 
         // when & then
         mockMvc.perform(post("/api/members/signup")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(registrationDto)))
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$").value("이미 사용중인 이메일입니다."));
+                .andExpect(jsonPath("$.message").value("이미 사용중인 이메일입니다."));
     }
 
     @Test
