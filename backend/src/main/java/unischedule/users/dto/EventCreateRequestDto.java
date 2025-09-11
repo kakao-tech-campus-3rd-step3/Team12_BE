7fc20f3b52d5fab045d092858712ab1ae4c4a698
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-09-11 12:58:23 +0900

    feat(user/events): 일정 추가 요청 validation 추가 #19

--- backend/src/main/java/unischedule/users/dto/EventCreateRequestDto.java
+++ backend/src/main/java/unischedule/users/dto/EventCreateRequestDto.java
@@ -1,8 +1,11 @@
 package unischedule.users.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-public record EventCreateRequestDto(String title, String description, LocalDateTime startTime,
-                                    LocalDateTime endTime, boolean isPrivate) {
+public record EventCreateRequestDto(
+    @NotBlank String title, @NotBlank String description, @NotNull LocalDateTime startTime,
+    @NotNull LocalDateTime endTime, boolean isPrivate) {
     
 }
