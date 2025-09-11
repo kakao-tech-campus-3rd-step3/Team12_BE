e5f0dd6c235d6a6cf6e1d888a3d5512d312772f6
sinsehwan <sk124590@gmail.com>
2025-09-11 15:52:39 +0900

    feat(auth): implement refreshToken issue, accessToken reissue logic #21

--- backend/src/main/java/unischedule/auth/repository/RefreshTokenRepository.java
+++ backend/src/main/java/unischedule/auth/repository/RefreshTokenRepository.java
@@ -2,6 +2,11 @@ package unischedule.auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import unischedule.auth.entity.RefreshToken;
+import unischedule.member.entity.Member;
+
+import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByMember(Member member);
 }
