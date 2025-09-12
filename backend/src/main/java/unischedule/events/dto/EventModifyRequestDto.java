e5ab4a1edd0e7f36f1acd7af3a152d9976c5230e
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-09-12 14:27:22 +0900

    feat: calendar crud 2 - 일정 수정 구현, 중복 일정 방지 (#24)
    * feat(upload): 신규 브랜치 생성 및 테스트 코드 오류 수정 #19
    
    * feat(events): CRUD 보충을 위한 Controller, Service 생성 #19
    
    * feat(events): 어노테이션 추가 및 분화 #19
    
    * feat(events): dto 추가 #19
    
    * feat(users/event): 수정 로직 추가 #19
    
    * feat(events): 일정 수정 구현 #19
    
    * feat(user/events): 일정 추가 요청 validation 추가 #19
    
    * feat(user/events): 일정 수정 로직 문제 수정 #19
    
    * fix(user/events): 코드 일부 수정 #19
    
    * feat(user/events): 테스트 코드 작성 및 수정 #19
    
    * fix(user/events): 일정 조회 조건 변경 #19
    
    * fix(user/events): Calendar에 Member 연결 #19
    
    * feat(user/events): 겹치는 일정 등록 못하도록 수정 #19
    
    * feat(user/events): 일정 수정에도 겹침 여부 확인 #19
    
    * fix(user/events): 인터페이스 제거 및 테스트 코드 수정 #19
    
    * chore(user/events): 줄바꿈 추가 #19
    
    * chore(user/events): 주석 #19
    
    * fix(user/events): 일정 조회에서 파라미터 부분 수정 #19
    
    * fix(user/events): 수정 부분 코드 수정 #19
    
    * docs(user/events): 주석을 통한 해야할 일 메모 #19

---
+++ backend/src/main/java/unischedule/events/dto/EventModifyRequestDto.java
@@ -0,0 +1,8 @@
+package unischedule.events.dto;
+
+import java.time.LocalDateTime;
+
+public record EventModifyRequestDto(String title, String description, LocalDateTime startTime,
+                                    LocalDateTime endTime, Boolean isPrivate) {
+    
+}
--- backend/src/main/java/unischedule/users/service/UsersService.java
+++ backend/src/main/java/unischedule/users/service/UsersService.java
@@ -1,14 +1,61 @@
 package unischedule.users.service;
 
+import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import unischedule.exception.InvalidInputException;
 import unischedule.users.dto.EventCreateRequestDto;
 import unischedule.users.dto.EventCreateResponseDto;
 import unischedule.users.dto.EventGetResponseDto;
+import unischedule.users.entity.Event;
+import unischedule.users.repository.EventRepository;
 
-public interface UsersService {
+@Service
+@RequiredArgsConstructor
+public class UsersService {
+    private final EventRepository eventRepository;
     
-    EventCreateResponseDto makeEvent(Long userId, EventCreateRequestDto requestDto);
+    @Transactional
+    public EventCreateResponseDto makeEvent(Long userId, EventCreateRequestDto requestDto) {
+        
+        boolean conflict = eventRepository.existsByCreatorIdAndStartAtLessThanAndEndAtGreaterThan(
+            userId,
+            requestDto.endTime(),
+            requestDto.startTime()
+        );
+        
+        if (conflict) {
+            throw new InvalidInputException("겹치는 일정이 있어 등록할 수 없습니다.");
+        }
+        
+        Event newEvent = Event.builder()
+            .creatorId(userId)
+            .title(requestDto.title())
+            .content(requestDto.description())
+            .startAt(requestDto.startTime())
+            .endAt(requestDto.endTime())
+            .state("CONFIRMED")
+            .isPrivate(requestDto.isPrivate())
+            .build();
+        
+        Event saved = eventRepository.save(newEvent);
+        
+        return new EventCreateResponseDto(saved);
+    }
     
-    List<EventGetResponseDto> getEvents(LocalDateTime startAt, LocalDateTime endAt, Long userId);
+    public List<EventGetResponseDto> getEvents(LocalDateTime startAt, LocalDateTime endAt, Long userId) {
+        List<Event> findEvents = eventRepository
+            .findByCreatorIdAndStartAtLessThanAndEndAtGreaterThan(
+                userId, endAt, startAt
+            );
+        
+        return findEvents.stream().map(
+            EventGetResponseDto::new
+        ).toList();
+    }
+    
+    //수정은 현재 테크 스펙 상 다른 도메인에 있어, 추후 추가 작성 필요
+    //삭제는 현재 테크 스펙 상 없음
 }
