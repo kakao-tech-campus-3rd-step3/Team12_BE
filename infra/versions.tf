bb1e957d5ece3c122d992ea5ef235e5d7b5564f1
hardjava <fkdlcn123@kumoh.ac.kr>
2025-09-12 18:50:45 +0900

    feat: 원격 상태 관리용 S3 버킷 추가

--- infra/versions.tf
+++ infra/versions.tf
@@ -7,4 +7,11 @@ terraform {
       version = "~> 6.11"
     }
   }
+
+  backend "s3" {
+    bucket       = "uni-schedule-remote-state-bucket"
+    key          = "terraform/state"
+    region       = "ap-northeast-2"
+    use_lockfile = true
+  }
 }
