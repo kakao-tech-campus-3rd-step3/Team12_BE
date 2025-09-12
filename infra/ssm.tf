ab8a644f5532737a0268112803cbad83659471ee
ssong <98401648+hardjava@users.noreply.github.com>
2025-09-12 14:48:29 +0900

    feat(infra): ECS 기반 Backend 배포 환경 구성 (#15)
    * feat(infra): lb, ecs security group 추가
    
    * feat(infra): ECS 클러스터 및 서비스 구성 추가
    
    * feat(infra): ECS 클러스터 및 서비스 구성 추가

--- infra/ssm.tf
+++ infra/ssm.tf
@@ -6,3 +6,7 @@ data "aws_ssm_parameter" "db_password" {
   name            = "/uni-schedule/config/db_password"
   with_decryption = true
 }
+
+data "aws_ssm_parameter" "db_url" {
+  name = "/uni-schedule/config/db_url"
+}
