6e17ec409c8ad3a67bdf0be1efcfa66e54ab6311
sinsehwan <sk124590@gmail.com>
2025-09-17 10:47:47 +0900

    chore(events): apply code formatting in eventRepository #37

--- backend/src/main/java/unischedule/events/repository/EventRepository.java
+++ backend/src/main/java/unischedule/events/repository/EventRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import unischedule.events.entity.Event;
 
@@ -14,10 +15,19 @@ public interface EventRepository extends JpaRepository<Event, Long> {
         LocalDateTime startAt
     );
 
-    @Query("SELECT e FROM Event e WHERE e.calendar.calendarId = :calendarId AND e.startAt <= :endAt AND e.endAt >= :startAt")
+    @Query("""
+            SELECT e
+            FROM Event e
+            WHERE e.calendar.calendarId = :calendarId
+            AND e.endAt >= :startAt
+            AND e.startAt <= :endAt
+    """)
     List<Event> findScheduleInPeriod(
+            @Param("calendarId")
             Long calendarId,
+            @Param("startAt")
             LocalDateTime startAt,
+            @Param("endAt")
             LocalDateTime endAt
     );
     
@@ -26,10 +36,19 @@ public interface EventRepository extends JpaRepository<Event, Long> {
         LocalDateTime startAt
     );
 
-    @Query("SELECT count(e) > 0 FROM Event e WHERE e.calendar.calendarId = :calendarId AND e.startAt <= :endAt AND e.endAt >= :startAt")
+    @Query("""
+            SELECT count(e) > 0
+            FROM Event e
+            WHERE e.calendar.calendarId = :calendarId
+            AND e.endAt >= :startAt
+            AND e.startAt <= :endAt
+    """)
     boolean existsScheduleInPeriod(
+            @Param("calendarId")
             Long calendarId,
+            @Param("startAt")
             LocalDateTime startAt,
+            @Param("endAt")
             LocalDateTime endAt
     );
 }
