b2e1e8032acc05057cad2f2807b4ae2c7c8e909a
sinsehwan <sk124590@gmail.com>
2025-09-18 19:18:11 +0900

    chore(event): apply jsonproperties in event request dtos #37

--- backend/src/main/java/unischedule/events/dto/EventCreateRequestDto.java
+++ backend/src/main/java/unischedule/events/dto/EventCreateRequestDto.java
@@ -1,21 +1,27 @@
 package unischedule.events.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public record EventCreateRequestDto(
+        @JsonProperty("calendar_id")
         @NotNull
         Long calendarId,
         @NotBlank
         String title,
         @NotBlank
         String description,
+        @JsonProperty("start_time")
         @NotNull
         LocalDateTime startTime,
+        @JsonProperty("end_time")
         @NotNull
         LocalDateTime endTime,
-        boolean isPrivate
+        @JsonProperty("is_private")
+        @NotNull
+        Boolean isPrivate
 ) {
     
 }
--- backend/src/main/java/unischedule/events/dto/EventModifyRequestDto.java
+++ backend/src/main/java/unischedule/events/dto/EventModifyRequestDto.java
@@ -1,16 +1,21 @@
 package unischedule.events.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record EventModifyRequestDto(
+        @JsonProperty("event_id")
         @NotNull
         Long eventId,
         String title,
         String description,
+        @JsonProperty("start_time")
         LocalDateTime startTime,
+        @JsonProperty("end_time")
         LocalDateTime endTime,
+        @JsonProperty("is_private")
         Boolean isPrivate
 ) {
     
