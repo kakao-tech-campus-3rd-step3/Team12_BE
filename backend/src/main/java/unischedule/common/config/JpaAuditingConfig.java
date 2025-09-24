ee0401999ed4a29cd85b7cbd9131095f3c184375
sinsehwan <sk124590@gmail.com>
2025-09-24 19:42:55 +0900

    fix(config): apply auditorAware bean in jpaAuditingConfig #44

--- backend/src/main/java/unischedule/common/config/JpaAuditingConfig.java
+++ backend/src/main/java/unischedule/common/config/JpaAuditingConfig.java
@@ -1,9 +1,18 @@
 package unischedule.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import unischedule.member.entity.Member;
+import unischedule.member.repository.MemberRepository;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<Member> auditorProvider(MemberRepository memberRepository) {
+        return new AuditorAwareImpl(memberRepository);
+    }
 }
