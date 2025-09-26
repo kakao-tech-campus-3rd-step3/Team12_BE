8f0544e80be30c44a12baac40d7c5434067e7e10
sinsehwan <sk124590@gmail.com>
2025-09-26 11:37:06 +0900

    feat(calendar): implement finding team calendar logic #49

--- backend/src/main/java/unischedule/calendar/repository/CalendarRepository.java
+++ backend/src/main/java/unischedule/calendar/repository/CalendarRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import unischedule.calendar.entity.Calendar;
 import unischedule.member.entity.Member;
+import unischedule.team.entity.Team;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +14,6 @@ public interface CalendarRepository extends JpaRepository<Calendar, Long> {
     List<Calendar> findByOwner(Member owner);
 
     Optional<Calendar> findByOwnerAndTeamIsNull(Member owner);
+
+    Optional<Calendar> findByTeam(Team team);
 }
--- backend/src/main/java/unischedule/calendar/service/internal/CalendarDomainService.java
+++ backend/src/main/java/unischedule/calendar/service/internal/CalendarDomainService.java
@@ -7,6 +7,7 @@ import unischedule.calendar.entity.Calendar;
 import unischedule.calendar.repository.CalendarRepository;
 import unischedule.exception.EntityNotFoundException;
 import unischedule.member.entity.Member;
+import unischedule.team.entity.Team;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +24,11 @@ public class CalendarDomainService {
     public Calendar getMyPersonalCalendar(Member member) {
         return calendarRepository.findByOwnerAndTeamIsNull(member)
                 .orElseThrow(() -> new EntityNotFoundException("개인 캘린더를 찾을 수 없습니다."));
+    }
+
+    @Transactional(readOnly = true)
+    public Calendar getTeamCalendar(Team team) {
+        return calendarRepository.findByTeam(team)
+                .orElseThrow(() -> new EntityNotFoundException("팀 캘린더를 찾을 수 없습니다."));
     }
 }
