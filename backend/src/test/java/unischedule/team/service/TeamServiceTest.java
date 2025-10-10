e2cbcc368399967f8f07d6f6923e8c000aa30d5d
hardjava <fkdlcn123@kumoh.ac.kr>
2025-10-10 18:21:42 +0900

    fix: import 와일드카드 제거

--- backend/src/test/java/unischedule/team/service/TeamServiceTest.java
+++ backend/src/test/java/unischedule/team/service/TeamServiceTest.java
@@ -4,7 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 import java.util.List;
