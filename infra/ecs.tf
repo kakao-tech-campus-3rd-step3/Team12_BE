b3b15f58a4fc7cd7758be3a687846f356219aa87
hardjava <fkdlcn123@kumoh.ac.kr>
2025-10-17 21:18:49 +0900

    feat: ecs task에 redis 환경변수 추가

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -3,13 +3,13 @@ resource "aws_ecs_cluster" "this" {
 }
 
 resource "aws_ecs_task_definition" "this" {
-  family             = "uni-schedule-task"
+  family                   = "uni-schedule-task"
   requires_compatibilities = ["FARGATE"]
-  network_mode       = "awsvpc"
-  cpu                = "256"
-  memory             = "512"
-  execution_role_arn = aws_iam_role.ecs_task_execution.arn
-  task_role_arn      = aws_iam_role.ecs_task.arn
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  task_role_arn            = aws_iam_role.ecs_task.arn
 
   container_definitions = jsonencode([
     {
@@ -27,14 +27,18 @@ resource "aws_ecs_task_definition" "this" {
       environment = [
         { name = "SPRING_DATASOURCE_URL", value = data.aws_ssm_parameter.db_url.value },
         { name = "JWT_ACCESS_TOKEN_TIMEOUT_SEC", value = data.aws_ssm_parameter.jwt_access_token_timeout_sec.value },
-        { name = "JWT_REFRESH_TOKEN_TIMEOUT_SEC", value = data.aws_ssm_parameter.jwt_refresh_token_timeout_sec.value }
+        { name = "JWT_REFRESH_TOKEN_TIMEOUT_SEC", value = data.aws_ssm_parameter.jwt_refresh_token_timeout_sec.value },
+        { name = "REDIS_HOST", value = data.aws_ssm_parameter.redis_host.value },
+        { name = "REDIS_USERNAME", value = data.aws_ssm_parameter.redis_username.value },
+        { name = "REDIS_PORT", value = "17830" }
       ]
 
       secrets = [
         { name = "SPRING_DATASOURCE_USERNAME", valueFrom = data.aws_ssm_parameter.db_username.arn },
         { name = "SPRING_DATASOURCE_PASSWORD", valueFrom = data.aws_ssm_parameter.db_password.arn },
         { name = "JWT_SECRET", valueFrom = data.aws_ssm_parameter.jwt_secret.arn },
-        { name = "OPENAI_API_KEY", valueFrom = data.aws_ssm_parameter.openai_api_key.arn }
+        { name = "OPENAI_API_KEY", valueFrom = data.aws_ssm_parameter.openai_api_key.arn },
+        { name = "REDIS_PASSWORD", valueFrom = data.aws_ssm_parameter.redis_password.arn }
       ]
 
       logConfiguration = {
@@ -53,7 +57,7 @@ resource "aws_lb" "this" {
   name               = "uni-schedule-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.lb.id]
+  security_groups    = [aws_security_group.lb.id]
   subnets            = aws_subnet.public_subnet[*].id
 }
 
@@ -95,7 +99,7 @@ resource "aws_ecs_service" "this" {
 
   network_configuration {
     subnets          = aws_subnet.public_subnet[*].id
-    security_groups = [aws_security_group.ecs_instance.id]
+    security_groups  = [aws_security_group.ecs_instance.id]
     assign_public_ip = true
   }
 
