d39c3abb07c7c94405a59b1a08b68eccf2d9de77
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-10-30 13:59:13 +0900

    feat: 테스트 코드 작성 #87

--- backend/src/test/java/unischedule/util/TestUtil.java
+++ backend/src/test/java/unischedule/util/TestUtil.java
@@ -1,5 +1,7 @@
 package unischedule.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import unischedule.calendar.entity.Calendar;
 import unischedule.events.domain.Event;
 import unischedule.events.domain.EventState;
@@ -10,6 +12,7 @@ import unischedule.team.domain.TeamMember;
 import unischedule.team.domain.TeamRole;
 
 import java.time.LocalDateTime;
+import unischedule.team.domain.WhenToMeet;
 
 public class TestUtil {
 
@@ -71,6 +74,17 @@ public class TestUtil {
 
     public static TeamMember makeTeamMember(Team team, Member member) {
         return new TeamMember(team, member, TeamRole.MEMBER);
+    }
+    
+    public static List<WhenToMeet> createInitialSlots(LocalDateTime start, LocalDateTime end, long totalMembers) {
+        List<WhenToMeet> slots = new ArrayList<>();
+        LocalDateTime cursor = start;
+        while (cursor.isBefore(end)) {
+            LocalDateTime slotEnd = cursor.plusMinutes(15);
+            slots.add(new WhenToMeet(cursor, slotEnd, totalMembers));
+            cursor = slotEnd;
+        }
+        return slots;
     }
 }
 
