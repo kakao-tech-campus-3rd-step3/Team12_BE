d0b2501520866d6d7e0358d9867e2e66756d2561
ssong <98401648+hardjava@users.noreply.github.com>
2025-10-31 23:14:43 +0900

    feat: 백엔드 HTTPS 적용 및 ALB 리다이렉트 설정 (#106)

--- infra/security_groups.tf
+++ infra/security_groups.tf
@@ -12,9 +12,9 @@ resource "aws_security_group" "database_sg" {
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -27,16 +27,23 @@ resource "aws_security_group" "lb" {
   vpc_id = aws_vpc.this.id
 
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -49,16 +56,16 @@ resource "aws_security_group" "ecs_instance" {
   vpc_id = aws_vpc.this.id
 
   ingress {
-    from_port = 8080
-    to_port   = 8080
-    protocol  = "tcp"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
     security_groups = [aws_security_group.lb.id]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
