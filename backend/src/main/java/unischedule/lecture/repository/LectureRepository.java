5fa1e9f249646902fc7d9a824de24520f1d050e4
Junyoung Yang <y@jun0.dev>
2025-11-01 21:40:43 +0900

    fix: 더 이상 없는 isPrivate 메서드 호출 제거

--- backend/src/main/java/unischedule/lecture/repository/LectureRepository.java
+++ backend/src/main/java/unischedule/lecture/repository/LectureRepository.java
@@ -1,8 +1,6 @@
 package unischedule.lecture.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import unischedule.lecture.domain.Lecture;
 
@@ -12,7 +10,6 @@ import java.util.List;
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
     
-    @Query("SELECT l FROM Lecture l WHERE l.event.calendar.owner.memberId = :memberId AND l.endDate >= :today")
-    List<Lecture> findActiveLecturesByMemberId(@Param("memberId") Long memberId, @Param("today") LocalDate today);
+    List<Lecture> findByEventCalendarOwnerMemberIdAndEndDateGreaterThanEqual(Long memberId, LocalDate today);
 }
 
