29bd206e68cd09aa80dec203e72398c2bec29a5f
Junyoung Yang <y@jun0.dev>
2025-11-01 21:11:21 +0900

    feat: LectureService 및 LectureRawService 구현

---
+++ backend/src/main/java/unischedule/lecture/service/internal/LectureRawService.java
@@ -0,0 +1,39 @@
+package unischedule.lecture.service.internal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import unischedule.exception.EntityNotFoundException;
+import unischedule.lecture.domain.Lecture;
+import unischedule.lecture.repository.LectureRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LectureRawService {
+    private final LectureRepository lectureRepository;
+    
+    @Transactional
+    public Lecture saveLecture(Lecture lecture) {
+        return lectureRepository.save(lecture);
+    }
+    
+    @Transactional(readOnly = true)
+    public Lecture findLectureById(Long lectureId) {
+        return lectureRepository.findById(lectureId)
+                .orElseThrow(() -> new EntityNotFoundException("강의를 찾을 수 없습니다."));
+    }
+    
+    @Transactional(readOnly = true)
+    public List<Lecture> findActiveLecturesByMemberId(Long memberId) {
+        return lectureRepository.findActiveLecturesByMemberId(memberId, LocalDate.now());
+    }
+    
+    @Transactional
+    public void deleteLecture(Lecture lecture) {
+        lectureRepository.delete(lecture);
+    }
+}
+
