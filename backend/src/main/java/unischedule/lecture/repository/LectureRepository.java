e8edebd1c3a65b84fbba08815fbd254037e179b1
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-11-03 20:31:28 +0900

    fix: 다가오는 일정 강의 필터링 부분 수정 #125

--- backend/src/main/java/unischedule/lecture/repository/LectureRepository.java
+++ backend/src/main/java/unischedule/lecture/repository/LectureRepository.java
@@ -1,6 +1,9 @@
 package unischedule.lecture.repository;
 
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import unischedule.lecture.domain.Lecture;
 
@@ -10,8 +13,12 @@ import java.util.List;
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
     
-    @Query("SELECT CASE WHEN COUNT(l) > 0 THEN true ELSE false END FROM Lecture l WHERE l.event.eventId = :eventId")
-    boolean existsByEventId(@Param("eventId") Long eventId);
+    @Query("""
+    SELECT l.event.eventId
+    FROM Lecture l
+    where l.event.calendar.owner.email = :email
+    """)
+    Set<Long> findAllEventIds(String email);
   
     List<Lecture> findByEventCalendarOwnerMemberIdAndEndDateGreaterThanEqual(Long memberId, LocalDate today);
 }
